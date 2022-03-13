96cf580961174e9de25fcdbc52ce8cacd0e24740
Christopher Kormanyos <ckormanyos@yahoo.com>
2022-03-13 12:03:12 +0100

    Add sanitize runs to CI

--- mandelbrot/cfg/mandelbrot_cfg_MANDELBROT_05_SEAHORSES.h
+++ mandelbrot/cfg/mandelbrot_cfg_MANDELBROT_05_SEAHORSES.h
@@ -1,4 +1,4 @@
-///////////////////////////////////////////////////////////////////////////////
+﻿///////////////////////////////////////////////////////////////////////////////
 //      Copyright Christopher Kormanyos 2022.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
@@ -8,12 +8,26 @@
 #ifndef MANDELBROT_CFG_MANDELBROT_05_SEAHORSES_2022_02_24_H
   #define MANDELBROT_CFG_MANDELBROT_05_SEAHORSES_2022_02_24_H
 
+  #if defined(__clang__)
+    #if defined __has_feature && (__has_feature(thread_sanitizer) || __has_feature(address_sanitizer))
+    #define MANDELBROT_TEST_OPTION_REDUCE_TEST_DEPTH
+    #endif
+  #elif defined(__GNUC__)
+    #if defined(__SANITIZE_THREAD__) || defined(__SANITIZE_ADDRESS__)
+    #define MANDELBROT_TEST_OPTION_REDUCE_TEST_DEPTH
+    #endif
+  #endif
+
   // This is a swirly seahorse image.
 
   #define MANDELBROT_FILENAME_STRING            "MANDELBROT_05_SEAHORSES"
 
   #define MANDELBROT_CALCULATION_DIGITS10       37
+  #if !defined(MANDELBROT_TEST_OPTION_REDUCE_TEST_DEPTH)
   #define MANDELBROT_CALCULATION_PIXELS_1D      2048UL
+  #else
+  #define MANDELBROT_CALCULATION_PIXELS_1D       256UL
+  #endif
   #define MANDELBROT_CALCULATION_ITERATIONS     2000UL
 
   #define MANDELBROT_POINT_DX_HALF              "1.76E-12"
