9b517e5c4e2c5f36888c92d6ed403e8f2fccc0bb
ckormanyos <ckormanyos@yahoo.com>
2023-02-26 13:37:09 +0100

    Do a more significant refactor of parallel_for

--- concurrency/parallel_for.h
+++ concurrency/parallel_for.h
@@ -21,7 +21,7 @@
                       callable_function_type parallel_function) -> void
     {
       // Estimate the number of threads available.
-      static const auto number_of_threads_hint =
+      const auto number_of_threads_hint =
         static_cast<signed>
         (
           std::thread::hardware_concurrency()
@@ -37,60 +37,64 @@
           )
         );
 
-      // Set the size of a slice for the range functions.
-      const auto n =
-        static_cast<index_type>
-        (
-          static_cast<index_type>(end - start) + static_cast<index_type>(1)
-        );
-
-      const auto slice =
-        (std::max)
-        (
-          static_cast<index_type>(std::round(static_cast<float>(n) / static_cast<float>(number_of_threads))),
-          static_cast<index_type>(1)
-        );
-
-      // Inner loop.
-      const auto launch_range =
-        [&parallel_function](index_type index_lo, index_type index_hi)
-        {
-          for(auto i = index_lo; i < index_hi; ++i) // NOLINT(altera-id-dependent-backward-branch)
-          {
-            parallel_function(i);
-          }
-        };
+      using thread_vector_type = std::vector<std::thread>;
 
       // Create the thread pool and launch the jobs.
-      std::vector<std::thread> pool { };
+      thread_vector_type pool { };
 
-      pool.reserve(number_of_threads);
-
-      auto i1 = start;
+      pool.reserve(static_cast<typename thread_vector_type::size_type>(number_of_threads));
 
       {
-        auto i2 = (std::min)(static_cast<index_type>(start + slice), end);
-
-        for(auto                         i = static_cast<index_type>(0U);
-                 static_cast<index_type>(i + static_cast<index_type>(INT8_C(1))) < static_cast<index_type>(number_of_threads);
-                                       ++i)
-        {
-          pool.emplace_back(launch_range, i1, i2);
-
-          i1 = i2;
-
-          if(i1 >= end)
+        // Inner loop.
+        const auto launch_range =
+          [&parallel_function](index_type index_lo, index_type index_hi)
           {
-            break;
+            for(auto i = index_lo; i < index_hi; ++i) // NOLINT(altera-id-dependent-backward-branch)
+            {
+              parallel_function(i);
+            }
+          };
+
+        auto i1 = start;
+
+        {
+          // Set the size of a slice for the range functions.
+          const auto n =
+            static_cast<index_type>
+            (
+              static_cast<index_type>(end - start) + static_cast<index_type>(1)
+            );
+
+          const auto slice =
+            (std::max)
+            (
+              static_cast<index_type>(std::round(static_cast<float>(n) / static_cast<float>(number_of_threads))),
+              static_cast<index_type>(1)
+            );
+
+          auto i2 = (std::min)(static_cast<index_type>(start + slice), end);
+
+          for(auto                         i = static_cast<index_type>(0U);
+                   static_cast<index_type>(i + static_cast<index_type>(INT8_C(1))) < static_cast<index_type>(number_of_threads);
+                                         ++i)
+          {
+            pool.emplace_back(launch_range, i1, i2);
+
+            i1 = i2;
+
+            if(i1 >= end)
+            {
+              break;
+            }
+
+            i2 = (std::min)(static_cast<index_type>(i2 + slice), end);
           }
-
-          i2 = (std::min)(static_cast<index_type>(i2 + slice), end);
         }
-      }
 
-      if(i1 < end)
-      {
-        pool.emplace_back(launch_range, i1, end);
+        if(i1 < end)
+        {
+          pool.emplace_back(launch_range, i1, end);
+        }
       }
 
       // Wait for the jobs to finish.
