2c412283e8bbd3ca2e5d18a55dacff68a67682ac
ckormanyos <ckormanyos@yahoo.com>
2024-04-06 09:48:45 +0200

    Add codes 60 satelite region pics TODO

--- mandelbrot/cfg/mandelbrot_cfg_MANDELBROT_60_SATELITE_REGION_01_magnify51.h
+++ mandelbrot/cfg/mandelbrot_cfg_MANDELBROT_60_SATELITE_REGION_01_magnify51.h
@@ -5,16 +5,16 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#ifndef MANDELBROT_CFG_MANDELBROT_60_SATELITE_REGION_01_2024_05_02_H
-  #define MANDELBROT_CFG_MANDELBROT_60_SATELITE_REGION_01_2024_05_02_H
+#ifndef MANDELBROT_CFG_MANDELBROT_60_SATELITE_REGION_01_MAGNIFY51_2024_05_02_H
+  #define MANDELBROT_CFG_MANDELBROT_60_SATELITE_REGION_01_MAGNIFY51_2024_05_02_H
 
-  constexpr char MANDELBROT_FILENAME_STRING[]       = "MANDELBROT_60_SATELITE_REGION_01";
+  constexpr char MANDELBROT_FILENAME_STRING[]       = "MANDELBROT_60_SATELITE_REGION_01_magnify51";
 
-  constexpr int  MANDELBROT_CALCULATION_DIGITS10    =     128;
+  constexpr int  MANDELBROT_CALCULATION_DIGITS10    =      76;
   constexpr int  MANDELBROT_CALCULATION_PIXELS_1D   =    2048;
-  constexpr int  MANDELBROT_CALCULATION_ITERATIONS  =   60000;
+  constexpr int  MANDELBROT_CALCULATION_ITERATIONS  =   50000;
 
-  constexpr char MANDELBROT_POINT_DX_HALF[]         = "8.1E-95";
+  constexpr char MANDELBROT_POINT_DX_HALF[]         = "1.4E-51";
 
   // This iteration stops at a little Mandelbrot figure
   // at a magnification of approximately 10^{-104}
@@ -24,4 +24,4 @@
 
   #include <mandelbrot/cfg/mandelbrot_cfg.h>
 
-#endif // MANDELBROT_CFG_MANDELBROT_60_SATELITE_REGION_01_2024_05_02_H
+#endif // MANDELBROT_CFG_MANDELBROT_60_SATELITE_REGION_01_MAGNIFY51_2024_05_02_H
