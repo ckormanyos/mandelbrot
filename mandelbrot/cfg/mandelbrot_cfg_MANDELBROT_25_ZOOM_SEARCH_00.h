0df98bc83b2aa6859a2cd008576c6704475de87e
ckormanyos <ckormanyos@yahoo.com>
2024-04-08 06:49:09 +0200

    Continue but not conclude 25 deep search

--- mandelbrot/cfg/mandelbrot_cfg_MANDELBROT_25_ZOOM_SEARCH_00.h
+++ mandelbrot/cfg/mandelbrot_cfg_MANDELBROT_25_ZOOM_SEARCH_00.h
@@ -8,16 +8,15 @@
 #ifndef MANDELBROT_CFG_MANDELBROT_25_ZOOM_SEARCH_00_2024_04_07_H
   #define MANDELBROT_CFG_MANDELBROT_25_ZOOM_SEARCH_00_2024_04_07_H
 
-  constexpr char MANDELBROT_FILENAME_STRING[]       = "MANDELBROT_25_ZOOM_SEARCH";
+  constexpr char MANDELBROT_FILENAME_STRING[]       = "MANDELBROT_25_ZOOM_SEARCH_00";
 
-  constexpr int  MANDELBROT_CALCULATION_DIGITS10    =      42;
-  constexpr int  MANDELBROT_CALCULATION_PIXELS_1D   =    1536;
-  constexpr int  MANDELBROT_CALCULATION_ITERATIONS  =   40000;
+  constexpr int  MANDELBROT_CALCULATION_DIGITS10    =      36;
+  constexpr int  MANDELBROT_CALCULATION_PIXELS_1D   =    2048;
+  constexpr int  MANDELBROT_CALCULATION_ITERATIONS  =    3000;
 
-  //constexpr char MANDELBROT_POINT_DX_HALF[]         = "1.1E-18";
-  constexpr char MANDELBROT_POINT_DX_HALF[]         = "1.1E-13";
-  constexpr char MANDELBROT_POINT_CENTER_X[]        = "+0.360964144650305208600";
-  constexpr char MANDELBROT_POINT_CENTER_Y[]        = "-0.615180770532733982050";
+  constexpr char MANDELBROT_POINT_DX_HALF[]         = "1.1E-20";
+  constexpr char MANDELBROT_POINT_CENTER_X[]        = "+0.36478004994591064742050";
+  constexpr char MANDELBROT_POINT_CENTER_Y[]        = "-0.62947785570505732472200";
 
   #include <mandelbrot/cfg/mandelbrot_cfg.h>
 
