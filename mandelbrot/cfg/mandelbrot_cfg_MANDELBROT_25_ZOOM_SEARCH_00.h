d40c0028a9d3719e746cc9c513932d9384e5677e
Christopher Kormanyos <ckormanyos@yahoo.com>
2024-04-08 15:23:53 +0200

    Interesting point found but high-res pic TODO

--- mandelbrot/cfg/mandelbrot_cfg_MANDELBROT_25_ZOOM_SEARCH_00.h
+++ mandelbrot/cfg/mandelbrot_cfg_MANDELBROT_25_ZOOM_SEARCH_00.h
@@ -10,13 +10,20 @@
 
   constexpr char MANDELBROT_FILENAME_STRING[]       = "MANDELBROT_25_ZOOM_SEARCH_00";
 
-  constexpr int  MANDELBROT_CALCULATION_DIGITS10    =      36;
+  constexpr int  MANDELBROT_CALCULATION_DIGITS10    =      58;
   constexpr int  MANDELBROT_CALCULATION_PIXELS_1D   =    2048;
-  constexpr int  MANDELBROT_CALCULATION_ITERATIONS  =    3000;
+  constexpr int  MANDELBROT_CALCULATION_ITERATIONS  =    8000;
 
-  constexpr char MANDELBROT_POINT_DX_HALF[]         = "1.1E-20";
-  constexpr char MANDELBROT_POINT_CENTER_X[]        = "+0.36478004994591064742050";
-  constexpr char MANDELBROT_POINT_CENTER_Y[]        = "-0.62947785570505732472200";
+  // There are countless points to iterate that dive on and on,
+  // deeper and deeper without showing ant interesting variation.
+  // There are, however, some interesting scenes such as this one.
+  // The image resembles huge towers or wobbly columns
+  // with meandering roads or streams below.
+
+  constexpr char MANDELBROT_POINT_DX_HALF[]         = "1.1E-32";
+  //constexpr char MANDELBROT_POINT_DX_HALF[]         = "1.1E-36";
+  constexpr char MANDELBROT_POINT_CENTER_X[]        = "+0.364780049945910647420847479526784741020";
+  constexpr char MANDELBROT_POINT_CENTER_Y[]        = "-0.629477855705057324723497932015414892010";
 
   #include <mandelbrot/cfg/mandelbrot_cfg.h>
 
