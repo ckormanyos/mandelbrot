e060e0002a97f5c6cb59f949f3926b4d197e7500
Christopher Kormanyos <ckormanyos@yahoo.com>
2024-04-12 19:27:11 +0200

    Refactor mandelbrot discovery to header-only

--- mandelbrot/cfg/mandelbrot_cfg_MANDELBROT_01_FULL.h
+++ mandelbrot/cfg/mandelbrot_cfg_MANDELBROT_01_FULL.h
@@ -16,8 +16,8 @@
   constexpr int  MANDELBROT_CALCULATION_PIXELS_1D   =    2048;
   constexpr int  MANDELBROT_CALCULATION_ITERATIONS  =    2000;
 
-  constexpr char MANDELBROT_POINT_DX_HALF[]         = "1.25";
-  constexpr char MANDELBROT_POINT_CENTER_X[]        = "0.75";
+  constexpr char MANDELBROT_POINT_DX_HALF[]         = "+1.25";
+  constexpr char MANDELBROT_POINT_CENTER_X[]        = "-0.75";
   constexpr char MANDELBROT_POINT_CENTER_Y[]        = "0.0";
 
   #include <mandelbrot/cfg/mandelbrot_cfg.h>
