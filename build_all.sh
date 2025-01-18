053727ea39876a14aadee124ef6461992a7c08dc
ckormanyos <ckormanyos@yahoo.com>
2025-01-18 10:41:33 +0100

    Bench ready for dbl-dbl, flt128 and dec-flt

--- build_all.sh
+++ build_all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-#  Copyright Christopher Kormanyos 2024.
+#  Copyright Christopher Kormanyos 2024 - 2025.
 #  Distributed under the Boost Software License,
 #  Version 1.0. (See accompanying file LICENSE_1_0.txt
 #  or copy at http://www.boost.org/LICENSE_1_0.txt)
 #
 
 # cd /mnt/c/Users/ckorm/Documents/Ks/PC_Software/NumericalPrograms/mandelbrot
-# ./build_all.sh --boost=/mnt/c/boost/boost_1_87_0 --my_cc=g++ --stdcc=c++14
+# ./build_all.sh --boost=/mnt/c/boost/boost_1_87_0 --my_cc=g++ --stdcc=gnu++20
 
 boost=
 gcc=g++
@@ -67,10 +67,11 @@ echo
 echo 'compile test/test_mandelbrot.cpp to test_mandelbrot.o'
 $MY_GCC -x c++ -c -finline-functions -march=native -mtune=native -O3 -Wall -Wextra -Wmissing-include-dirs -std=$MY_STD -I. -Ipng/zlib/zlib-1.3.1.1-2024 -Ipng/libpng/libpng-1.6.44.git-2024 -Ijpeg/jpeg-6b-2022 $MY_BOOST_INC -pthread test/test_mandelbrot.cpp -o test_mandelbrot.o
 #$MY_GCC -x c++ -c -finline-functions -march=native -mtune=native -O3 -Wall -Wextra -Wmissing-include-dirs -std=$MY_STD -I. -Ipng/zlib/zlib-1.3.1.1-2024 -Ipng/libpng/libpng-1.6.44.git-2024 -Ijpeg/jpeg-6b-2022 -I/mnt/c/ChrisGitRepos/boost_gsoc2021/multiprecision/include $MY_BOOST_INC -DMANDELBROT_USE_DOUBLE_DOUBLE -pthread test/test_mandelbrot.cpp -o test_mandelbrot.o
+#$MY_GCC -x c++ -c -finline-functions -march=native -mtune=native -O3 -Wall -Wextra -Wmissing-include-dirs -std=$MY_STD -I. -Ipng/zlib/zlib-1.3.1.1-2024 -Ipng/libpng/libpng-1.6.44.git-2024 -Ijpeg/jpeg-6b-2022 -I/mnt/c/ChrisGitRepos/boost_gsoc2021/multiprecision/include $MY_BOOST_INC -DMANDELBROT_USE_FLOAT128 -pthread test/test_mandelbrot.cpp -o test_mandelbrot.o
 echo
 
 echo 'link test_mandelbrot.o with libraries to create test_mandelbrot.exe'
-$MY_GCC -x none -march=native -mtune=native test_mandelbrot.o ./png/libpng/libpng-1.6.44.git-2024/obj/libpng16.a ./png/zlib/zlib-1.3.1.1-2024/obj/libz.a ./jpeg/jpeg-6b-2022/obj/libjpeg-6b.a -lpthread -o test_mandelbrot.exe
+$MY_GCC -x none -march=native -mtune=native test_mandelbrot.o ./png/libpng/libpng-1.6.44.git-2024/obj/libpng16.a ./png/zlib/zlib-1.3.1.1-2024/obj/libz.a ./jpeg/jpeg-6b-2022/obj/libjpeg-6b.a -lpthread -lquadmath -o test_mandelbrot.exe
 echo
 
 echo 'verify existence of test_mandelbrot.exe'
--- mandelbrot/cfg/mandelbrot_cfg.h
+++ mandelbrot/cfg/mandelbrot_cfg.h
@@ -1,5 +1,5 @@
 ﻿///////////////////////////////////////////////////////////////////////////////
-//      Copyright Christopher Kormanyos 2022 - 2024.
+//      Copyright Christopher Kormanyos 2022 - 2025.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
@@ -43,6 +43,14 @@
   #include <boost/multiprecision/cpp_double_fp.hpp>
 
   #define MANDELBROT_COORD_PNT_NUMBER_TYPE(MB_DIGITS10) ::boost::multiprecision::cpp_double_double // NOLINT(cppcoreguidelines-macro-usage)
+
+  #define MANDELBROT_ITERATION_NUMBER_TYPE(MB_DIGITS10) MANDELBROT_COORD_PNT_NUMBER_TYPE(MB_DIGITS10) // NOLINT(cppcoreguidelines-macro-usage)
+
+  #elif defined(MANDELBROT_USE_FLOAT128)
+
+  #include <boost/multiprecision/float128.hpp>
+
+  #define MANDELBROT_COORD_PNT_NUMBER_TYPE(MB_DIGITS10) ::boost::multiprecision::float128 // NOLINT(cppcoreguidelines-macro-usage)
 
   #define MANDELBROT_ITERATION_NUMBER_TYPE(MB_DIGITS10) MANDELBROT_COORD_PNT_NUMBER_TYPE(MB_DIGITS10) // NOLINT(cppcoreguidelines-macro-usage)
 
