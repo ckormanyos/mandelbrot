1743926896755001593ba1bb5a10bda12920ee43
ckormanyos <ckormanyos@yahoo.com>
2025-01-19 22:25:44 +0100

    Update syntax and copyrights

--- mandelbrot/mandelbrot.h
+++ mandelbrot/mandelbrot.h
@@ -1,5 +1,5 @@
 ﻿///////////////////////////////////////////////////////////////////////////////
-// Copyright Christopher Kormanyos 2015 - 2024.
+// Copyright Christopher Kormanyos 2015 - 2025.
 // Copyright Sebastian Streubel 2024.
 // Distributed under the Boost Software License,
 // Version 1.0. (See accompanying file LICENSE_1_0.txt
