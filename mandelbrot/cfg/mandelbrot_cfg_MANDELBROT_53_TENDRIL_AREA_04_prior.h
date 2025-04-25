b23bcae3002d626135b66bb4692603f3437bfec7
ckormanyos <ckormanyos@yahoo.com>
2025-04-25 20:07:27 +0200

    Add more nice images

--- mandelbrot/cfg/mandelbrot_cfg_MANDELBROT_53_TENDRIL_AREA_04_prior.h
+++ mandelbrot/cfg/mandelbrot_cfg_MANDELBROT_53_TENDRIL_AREA_04_prior.h
@@ -1,12 +1,12 @@
 ﻿///////////////////////////////////////////////////////////////////////////////
-//      Copyright Christopher Kormanyos 2022 - 2025.
+//      Copyright Christopher Kormanyos 2025.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#ifndef MANDELBROT_CFG_MANDELBROT_XX_TRY_POINTS_2024_04_30_H
-  #define MANDELBROT_CFG_MANDELBROT_XX_TRY_POINTS_2024_04_30_H
+#ifndef MANDELBROT_CFG_MANDELBROT_53_TENDRIL_AREA_04_PRIOR_2025_04_25_H
+  #define MANDELBROT_CFG_MANDELBROT_53_TENDRIL_AREA_04_PRIOR_2025_04_25_H
 
   #if (!defined(_MSC_VER) && defined(__cplusplus) && (__cplusplus >= 201703L))
   namespace ckormanyos::mandelbrot::config {
@@ -14,17 +14,23 @@
   namespace ckormanyos { namespace mandelbrot { namespace config { // NOLINT(modernize-concat-nested-namespaces)
   #endif
 
-  // This is a specialized configuration file intended to be used
-  // for simply trying out deep dives, such as those found by the
-  // MandelbrotDiscovery program or found by other methods.
+  constexpr char MANDELBROT_FILENAME_STRING[]       = "MANDELBROT_53_TENDRIL_AREA_04_prior";
 
-  constexpr char MANDELBROT_FILENAME_STRING[]       = "MANDELBROT_XX_TRY_POINTS";
-
-  constexpr int  MANDELBROT_COORD_PNT_DIGITS10      =     168;
+  constexpr int  MANDELBROT_COORD_PNT_DIGITS10      =     162;
   constexpr int  MANDELBROT_ITERATION_DIGITS10      =      28;
   constexpr int  MANDELBROT_CALCULATION_PIXELS_X    =    2048;
   constexpr int  MANDELBROT_CALCULATION_PIXELS_Y    =    2048;
   constexpr int  MANDELBROT_CALCULATION_ITERATIONS  =   20000;
+
+  // This was a thrilling point. It ends up hitting a Mandelbrot
+  // figure at around order 152. But prior to doing so, the image
+  // goes through a variety of geometric transformations. In between
+  // the transformations it looks like a whirlpool spinning around.
+
+  // In mandelbrot_cfg_MANDELBROT_53_TENDRIL_AREA_04, I had walked this
+  // point back to Magnification order 10^138. The point here, however,
+  // is a few orders prior to that point and it can be found at a
+  // magnification around order 10^124.
 
   constexpr char MANDELBROT_POINT_DX_HALF[]         = "6.5E-124";
   constexpr char MANDELBROT_POINT_DY_HALF[]         = "6.5E-124";
@@ -42,4 +48,4 @@
   } // namespace ckormanyos
   #endif
 
-#endif // MANDELBROT_CFG_MANDELBROT_XX_TRY_POINTS_2024_04_30_H
+#endif // MANDELBROT_CFG_MANDELBROT_53_TENDRIL_AREA_04_PRIOR_2025_04_25_H
