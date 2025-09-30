fe8bc2e70d099fffaa245c9d0615dfb2d02e3149
ckormanyos <ckormanyos@yahoo.com>
2025-09-30 12:19:02 +0200

    Repair a typo

--- mandelbrot/mandelbrot_generator_perturbative.h
+++ mandelbrot/mandelbrot_generator_perturbative.h
@@ -168,7 +168,7 @@
             ++unordered_parallel_row_count;
 
             const float percent = (100.0F * static_cast<float>(unordered_parallel_row_count))
-                                  / static_cast<float>(y_coord.size();
+                                  / static_cast<float>(y_coord.size());
 
             std::stringstream strm { };
 
